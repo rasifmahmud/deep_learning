63e9a38427f55744ec66d5765d6d3a086a8e9071
rasifmahmud <rasifmahmud16@gmail.com>
2019-12-31 13:58:30 -0600

    Utils module added including a `NumPyMixin` that implements different utility function related to numpy

---
+++ utils/numpy_mixin.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class NumPyMixin:
+    @staticmethod
+    def get_zero_matrix(num_of_rows, num_of_colms):
+        weight_vector = np.zeros((num_of_rows, num_of_colms))
+        return weight_vector
+
+    @staticmethod
+    def get_random_matrix(num_of_rows, num_of_colms):
+        weight_vector = np.random.randn(num_of_rows, num_of_colms)
+        return weight_vector
+
+    @staticmethod
+    def calculate_sigmoid(x):
+        return 1 / (1 + np.exp(-x))
